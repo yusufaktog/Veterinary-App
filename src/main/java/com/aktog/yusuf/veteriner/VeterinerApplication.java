482e23408bb661ce956eb6afdff67227718851e4
Yusuf Aktog <yusufaktok@gmail.com>
2022-02-09 12:26:34 +0300

    project skeleton has been created

---
+++ src/main/java/com/aktog/yusuf/veteriner/VeterinerApplication.java
@@ -0,0 +1,13 @@
+package com.aktog.yusuf.veteriner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aktog/yusuf/veteriner/VeterinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aktog.yusuf.veteriner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
